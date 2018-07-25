878b679eb866d4eb2012b55e59a7f657fa9a6e58
Akash Kundu <akashkundu92@gmail.com>
2018-07-24 23:58:15 -0700

    initial setup done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 //index.js
+
+var relativePathToLogFile = "squid_stuff/squid_access.log";
+
+function readLogFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+window.onload = function() {
+	readLogFile(relativePathToLogFile);
+};
